ef585967c2717f62f8cf5f9468e849beb8ea09c5
johny-kim <johny.kim@lge.com>
2019-07-10 16:08:54 +0900

    add ssl._create_unverified_context temporarily

--- master/test_client.py
+++ master/test_client.py
@@ -1,8 +1,8 @@
-import urllib2
-from OpenSSL import SSL
+import urllib2, ssl
+
 try: 
-    #response = urllib2.urlopen('https://localhost')
-    response = urllib2.urlopen('https://www.naver.com')
+    response = urllib2.urlopen('https://localhost', context=ssl._create_unverified_context())
+    #response = urllib2.urlopen('https://www.naver.com')
     print 'response headers: "%s"' % response.info() 
 except IOError, e: 
     if hasattr(e, 'code'): # HTTPError 
