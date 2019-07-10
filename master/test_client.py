36ee56cde62f9acfe2a2cfec32857b0d47118a18
johny-kim <johny.kim@lge.com>
2019-07-10 20:44:39 +0900

    success modification of https connection

--- master/test_client.py
+++ master/test_client.py
@@ -1,13 +1,24 @@
-import urllib2, ssl
+#!/bin/env python
+"""
+A simple example of using Python sockets for a client HTTPS connection.
+"""
 
-try: 
-    response = urllib2.urlopen('https://localhost', context=ssl._create_unverified_context())
-    #response = urllib2.urlopen('https://www.naver.com')
-    print 'response headers: "%s"' % response.info() 
-except IOError, e: 
-    if hasattr(e, 'code'): # HTTPError 
-        print 'http error code: ', e.code 
-    elif hasattr(e, 'reason'): # URLError 
-        print "can't connect, reason: ", e.reason 
-    else: 
-        raise
+import ssl
+import socket
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context.verify_mode = ssl.CERT_REQUIRED
+context.check_hostname = False #This is check for DNSNAME
+context.load_verify_locations("/home/pi/python_test/server.pem") #modify this path
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ssl_sock = context.wrap_socket(s, server_hostname='localhost')
+ssl_sock.connect(('localhost', 443))
+ssl_sock.sendall("GET / HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n")
+
+while True:
+    new = ssl_sock.recv(4096)
+    if not new:
+        ssl_sock.close()
+        break
+    print new
