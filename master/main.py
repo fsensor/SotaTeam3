525abbb6d03f631dbb0d5a652aa49643bf42650b
haejung401 <diary40122@gmail.com>
2019-07-11 22:22:46 -0900

    [raspberry] verify signature into main.py

--- master/main.py
+++ master/main.py
@@ -8,12 +8,14 @@ import datetime
 import hashlib
 from subprocess import Popen
 from subprocess import PIPE
+from OpenSSL import crypto
+from Crypto.Hash import SHA256
 
 tmp_current_imgfile_name = "sample_data_file"
 
 current_imgfile_name = "sample_data_file.signed"
 keyfile_name = "test.key" 
-cerfile_name = ''
+cerfile_name = 'test.crt'
 
 #server data
 server_version = 2
@@ -37,26 +39,30 @@ def sign_image(img_name):
   # read image file
   print "sign_image"
 
-  f = open(img_name, 'rb')
-  f.seek(0, 2)
-  imgfile_size = f.tell()
-  f.seek(0);
-  img_data = f.read(imgfile_size)
-  f.close()
-  # hash boot image
- 
-  img_hash = hashlib.sha256(img_data).digest()
-  # sign with private RSA key
-  p = Popen(["openssl", "rsautl", "-sign", "-inkey", keyfile_name], stdin = PIPE, stdout = PIPE)
-  rsa_sign = p.communicate(img_hash)[0]
-  # write signed boot image
+  with open(img_name, 'rb') as f:
+      f.seek(0, 2)
+      imgfile_size = f.tell()
+      f.seek(0);
+      img_data = f.read(imgfile_size)
 
+  # hash boot image
+  # img_hash = hashlib.sha256(img_data).digest()
+  img_hash = SHA256.new(img_data.encode('utf-8')).digest()
+
+  # sign with private RSA key
+  #p = Popen(["openssl", "rsautl", "-sign", "-inkey", keyfile_name], stdin = PIPE, stdout = PIPE)
+  #rsa_sign = p.communicate(img_hash)[0]
+  with open(keyfile_name, 'rb+') as f:
+      pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read())
+
+  rsa_sign = crypto.sign(pkey, img_hash, 'sha256')
+
+  # write signed boot image
   tmp = img_name + '.signed'
-  f = open(tmp, 'wb')
-  f.write(img_data)
-  f.write(rsa_sign)
-  f.close()
-  print "end signe_image"
+  with open(tmp, 'wb') as f:
+      f.write(img_data)
+      f.write(rsa_sign)
+  print "end sign_image"
   return True
 
 def read_current_image():
@@ -69,20 +75,20 @@ def read_current_image():
 
   print "read_current_image"
   print current_imgfile_name
-  f = open(current_imgfile_name, 'rb')
-  f.seek(0, 2)
-  imgfile_size = f.tell()  
-  f.seek(0);
-  img_data = f.read(imgfile_size)
-  f.seek(0)
-  current_magic = f.read(4)
-  tmp = f.read(4)
-  current_version = struct.unpack('i', tmp)
-  tmp = f.read(4)
-  current_body_len = struct.unpack('i', tmp)
-  current_body = f.read(current_body_len[0])
-  f.seek(imgfile_size-LGE_RSASIGN_SIZE)
-  current_sig = f.read(LGE_RSASIGN_SIZE)
+  with open(current_imgfile_name, 'rb') as f:
+      f.seek(0, 2)
+      imgfile_size = f.tell()  
+      f.seek(0);
+      img_data = f.read(imgfile_size)
+      f.seek(0)
+      current_magic = f.read(4)
+      tmp = f.read(4)
+      current_version = struct.unpack('i', tmp)
+      tmp = f.read(4)
+      current_body_len = struct.unpack('i', tmp)
+      current_body = f.read(current_body_len[0])
+      f.seek(imgfile_size-LGE_RSASIGN_SIZE)
+      current_sig = f.read(LGE_RSASIGN_SIZE)
 
   print current_magic
   print current_version[0]
@@ -93,7 +99,16 @@ def read_current_image():
   print type(current_sig)
   print current_sig.encode("hex")
 #sig = f.read(imgfile_size-)
-  f.close()
+
+def verify_signature(cert, sig, dgst):
+    try:
+        print('do verify')
+        crypto.verify(cert, sig, dgst, "sha256")
+        print('done verify')
+        return True
+    except:
+        print('verify func exception')
+        return False
 
 #----------------------------------------------------------------------------
 # firmware_update SCRIPT BEGIN
@@ -103,6 +118,22 @@ def firmware_update():
   global server_file_name_signed
   global current_imgfile_name
   print "verify"
+  
+  with open(cerfile_name, 'rb+') as f:
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
+
+  with open(server_file_name_signed, 'rb+') as f:
+      f.seek(0,2)
+      img_len = f.tell()
+      f.seek(0)
+      msg = f.read(img_len-LGE_RSASIGN_SIZE)
+      sig = f.read(LGE_RSASIGN_SIZE)
+      dgst = SHA256.new(msg.encode('utf-8')).digest()
+
+  if (verify_signature(cert, sig, dgst)):
+      print "verify ok"
+  else:
+      print "verify ng"
   print "change"
 
   os.remove(current_imgfile_name)
--- master/verif_sign/read_test.py
+++
@@ -1,5 +0,0 @@
-with open("data", 'rb+') as f:
-    two = f.read(2)
-    three = f.read(3)
-    print(two)
-    print(three)
