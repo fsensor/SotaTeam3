f830da9995d28ac14c75333e7fd146e22e46b14e
fsensor <fsensor>
2019-07-15 04:53:08 +0000

    Add script to run server for convenience.

---
+++ server/start_server.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Find path to this script
+
+if [ "$(echo $0 | grep -E -o '^\./')" == "./" ]
+then
+  absolute_path=$PWD/$(echo $0 | sed -E '{s/[^\/]+$//}' | sed -E '{s/^\.\///}')
+else
+  absolute_path=$(echo $0 | sed -E '{s/[^\/]+$//}')
+fi
+
+NODE_PATH=node-v12.6.0-linux-x64/bin
+APP_DIR=sotaserver
+export PATH=$absolute_path$NODE_PATH:$PATH
+cd $absolute_path$APP_DIR
+
+function showUsage {
+  echo "usage: " $(echo $0 | grep -E -o '[^\/]+$') "[option]"
+  echo "  option : d   run as daemon"
+}
+
+run_as_daemon=
+
+function checkOption {
+  for option in "$@"
+  do
+    echo process option $option
+    if [[ $option == "d" ]]
+    then
+      run_as_daemon=1
+    else
+     showUsage
+      exit
+    fi
+  done
+  echo 
+}
+
+if [ $# -ge 1 ]
+then
+  checkOption $@
+fi
+
+if [[ $run_as_daemon -eq 1 ]]
+then
+  echo Run as a daemon 
+  nohup npm start&
+else
+  echo Run
+  npm start 
+fi
