a77e71c6af0710fb1817a9d71dc0522f4ab481bf
fsensor <fsensor>
2019-07-17 12:51:29 +0900

    Support initd to invoke strongloop pm

--- server/prepare_server.sh
+++ server/prepare_server.sh
@@ -53,11 +53,14 @@ errorhandle $? "Fail to run mongodb daemon"
 SLP_SERVICE_NAME=sotaslpm
 SLP_SERVICE_FILE=$SLP_SERVICE_NAME.service
 REAL_SLP_SERVICE_FILE=strong-pm.service
+REAL_SLP_INITD_FILE=strong-pm
 SLP_SERVICE_SED_FILE=$REAL_SLP_SERVICE_FILE.sed
+SLP_INITD_SED_FILE=$REAL_SLP_INITD_FILE.sed
 SLP_DIR=strong-pm
 SLP_PATH=$absolute_path$SLP_DIR
 ESCAPED_SLP_PATH=$(echo $SLP | sed -E "")
 SYSTEMD_FILE_PATH=/lib/systemd/system/
+INITD_FILE_PATH=/etc/init.d/
 SLP_PM_HOME=$SLP_PATH/pm
 TO_BE_REPLACED=REPLACE_TO_REAL_PATH
 
@@ -67,22 +70,46 @@ then
   mkdir -p $SLP_PM_HOME
 fi
 
-# Replace environment
-if [ ! -f $SLP_PATH/systemd/$REAL_SLP_SERVICE_FILE ]
+# Set-up service file
+SLP_PM_HOME=$SLP_PATH/pm
+TO_BE_REPLACED=REPLACE_TO_REAL_PATH
+
+if [ ! -d $SLP_HOME ]
 then
-  echo "Generating StrongLoop PM service file"
-  sed -E "s#$TO_BE_REPLACED#$SLP_PM_HOME#" $SLP_PATH/systemd/$SLP_SERVICE_SED_FILE > $SLP_PATH/systemd/$REAL_SLP_SERVICE_FILE
+  echo "Create StrongLoop PM home dir"
+  mkdir -p $SLP_PM_HOME
 fi
 
+# Set-up service file
+systemctl &> /dev/null
 
-# First remove service file in default path
-echo "Update link"
-rm -f $SYSTEMD_FILE_PATH$SLP_SERVICE_FILE
-ln -s $SLP_PATH/systemd/$REAL_SLP_SERVICE_FILE $SYSTEMD_FILE_PATH$SLP_SERVICE_FILE
+if [ $? -ne 1 ]
+then
+  #systemctl is not available. Try to use init.d script
+  if [ ! -f $SLP_PATH/systemd/$REAL_SLP_INITD_FILE ]
+  then
+    echo "Generating StrongLoop PM initd file"
+    sed -E "s#$TO_BE_REPLACED#$absolute_path#" $SLP_PATH/systemd/$SLP_INITD_SED_FILE > $SLP_PATH/systemd/$REAL_SLP_INITD_FILE
+  fi   
+  echo "Update link"
+  rm -f $INITD_FILE_PATH$REAL_SLP_INITD_FILE
+  ln -s $SLP_PATH/systemd/$REAL_SLP_INITD_FILE $INITD_FILE_PATH$REAL_SLP_INITD_FILE
+else
+  #anyway systemctl is available even though it returns error
+  if [ ! -f $SLP_PATH/systemd/$REAL_SLP_SERVICE_FILE ]
+  then
+    echo "Generating StrongLoop PM service file"
+    sed -E "s#$TO_BE_REPLACED#$SLP_PM_HOME#" $SLP_PATH/systemd/$SLP_SERVICE_SED_FILE > $SLP_PATH/systemd/$REAL_SLP_SERVICE_FILE
+  fi
+  echo "Update link"
+  rm -f $SYSTEMD_FILE_PATH$SLP_SERVICE_FILE
+  ln -s $SLP_PATH/systemd/$REAL_SLP_SERVICE_FILE $SYSTEMD_FILE_PATH$SLP_SERVICE_FILE
+fi
 
+exit 
 
 echo "Run StrongLoop PM"
 service strong-pm start
-errorhandle $? "Fail to run mongodb daemon"
+errorhandle $? "Fail to run StrongLoop PM"
 
 echo "All done. Now you can invoke start_server.sh to run sota server!!"
