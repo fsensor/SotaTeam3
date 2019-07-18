31b683ae991c223888cb42beb32de2f3b9fa1f20
haejung401 <diary40122@gmail.com>
2019-07-18 08:00:52 +0100

    [raspberry] cert verification for slave is required

--- master/main.py
+++ master/main.py
@@ -10,6 +10,7 @@ import hashlib
 import ssl
 import socket
 from urllib.request import urlopen
+from urllib.request import HTTPError
 import json,ast,base64
 import http.server
 import threading
@@ -23,7 +24,7 @@ import binascii
 
 server_url="https://192.168.0.10:33341/"
 #server_url="https://localhost:33341/"
-master_addr="192.168.0.4"
+master_addr="178.5.1.1"
 tmp_current_imgfile_name = "sample_data_file"
 current_imgfile_name = "sample_data_file.signed"
 version_file_name = "version.signed"
@@ -39,7 +40,7 @@ server_keyfile_name = key_dir+'ServerCert/ServerPriv.pem'
 server_chain_name = key_dir+'ServerCert/ServerChain.pem'
 slave1_cerfile_name = key_dir+'./Slave1Cert/Slave1Cert.pem'
 slave1_keyfile_name = key_dir+'./Slave1Cert/Slave1Priv.pem'
-slave1_chain_name = key_dir+'./Slave1Cert/SlaveChain.pem'
+slave1_chain_name = key_dir+'./Slave1Cert/Slave1Chain.pem'
 slave2_cerfile_name = key_dir+'./Slave2Cert/Slave2Cert.pem'
 slave2_keyfile_name = key_dir+'./Slave2Cert/Slave2Priv.pem'
 slave2_chain_name = key_dir+'./Slave2Cert/Slave2Chain.pem'
@@ -50,21 +51,21 @@ server_file_name = "sample_data_file_server"
 server_file_name_signed = "sample_data_file_server.signed"
 #image structure
 current_magic = ''
-current_version = 0
+current_version = '0'
 current_body_len = 0
 current_body = ''
 current_sig = ''
 
 LGE_METADATA_HDR_MAGIC  = '\x4C\x47\x45\x31' # 'LGE!'
 LGE_SHA256_SIZE   = 32
-LGE_RSASIGN_SIZE  = 256
+LGE_RSASIGN_SIZE  = 512
 
 #----------------------------------------------------------------------------
 # Sign boot image
 #----------------------------------------------------------------------------
 def sign_image(img_name):
   # read image file
-  print (sign_image)
+  print ("sign_image")
 
   with open(img_name, 'rb') as f:
       f.seek(0, 2)
@@ -72,13 +73,11 @@ def sign_image(img_name):
       f.seek(0);
       img_data = f.read(imgfile_size)
 
-  img_hash = SHA256.new(str(img_data).encode('utf-8')).digest()
-
   # sign with private RSA key
   with open(keyfile_name, 'rb+') as f:
       pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read())
 
-  rsa_sign = crypto.sign(pkey, img_hash, 'sha256')
+  rsa_sign = crypto.sign(pkey, img_data, 'sha256')
 
   # write signed boot image
   tmp = img_name + '.signed'
@@ -144,16 +143,33 @@ def firmware_update():
   
   with open(cerfile_name, 'rb+') as f:
     cert = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
-    f.close()
   with open(server_file_name_signed, 'rb+') as f:
+  #with open("msg.bin.signed", 'rb+') as f:
       f.seek(0,2)
       img_len = f.tell()
       f.seek(0)
+      if img_len <= LGE_RSASIGN_SIZE:
+          print("update image file too short! ", img_len, "bytes")
+          return False
       msg = f.read(img_len-LGE_RSASIGN_SIZE)
       sig = f.read(LGE_RSASIGN_SIZE)
-      dgst = SHA256.new(str(msg).encode('utf-8')).digest()
-      f.close()
-  if (verify_signature(cert, sig, dgst)):
+#      dgst = SHA256.new(str(msg).encode('utf-8')).digest()
+      print("sig: ", sig)
+      print("msg: ", msg)
+#  with open("./sig.bin", 'wb') as f:
+#      f.write(sig)
+
+#  with open("./msg.bin", 'wb') as f:
+#      f.write(msg)
+
+#  with open(keyfile_name, 'rb+') as f:
+#      pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read())
+
+#  with open("./sig_py.bin", 'wb+') as f:
+#      sig_py = crypto.sign(pkey, msg, 'sha256')
+#      f.write(sig_py)
+
+  if (verify_signature(cert, sig, msg)):
       print ("verify ok")
   else:
       print ("verify ng")
@@ -172,11 +188,13 @@ def image_down():
   print ("image_down")
   global server_file_name_signed
   server_file_response = https_connection(server_url, "GetLatestImage")
+  if server_file_response is False:
+      return False
   content = json.loads(server_file_response)
   content = base64.b64decode(content)
   print("content: ", content)
-  f = open(server_file_name_signed, "w") #sample_data_file_server.signed
-  f.write(str(content))
+  f = open(server_file_name_signed, "wb") #sample_data_file_server.signed
+  f.write(content)
   f.close()
 #does it need to error handling?
 
@@ -188,6 +206,8 @@ def get_version_to_server():
   global server_version
 
   server_file_response = https_connection(server_url, "GetVersion")
+  if server_file_response is False:
+      return False
   content = json.loads(server_file_response)
   server_version = content['version'][7]
   sig_bin = binascii.unhexlify(content['sign'])
@@ -215,7 +235,14 @@ def https_connection(url, data):
   context.load_verify_locations(server_chain_name) #certificate file
   context.load_cert_chain(certfile=master_cerfile_name, keyfile=master_keyfile_name)
   print (url+data)
-  response = urlopen(url+data, context=context)
+  try:
+      response = urlopen(url+data, context=context)
+  except HTTPError as e:
+      print(e)
+      return False
+  except:
+      print('network error')
+      return False
   byte_data = response.read()
   text_data = byte_data.decode('utf-8')
   return text_data
@@ -227,7 +254,7 @@ def https_connection(url, data):
 def slave_connection():
   global master_addr
   httpsd = http.server.HTTPServer((master_addr, 33341), http.server.SimpleHTTPRequestHandler)
-  httpsd.socket = ssl.wrap_socket (httpsd.socket, certfile=master_cerfile_name, keyfile=master_keyfile_name, server_side=True)
+  httpsd.socket = ssl.wrap_socket (httpsd.socket, cert_reqs=ssl.CERT_REQUIRED, ca_certs=slave1_chain_name, certfile=master_cerfile_name, keyfile=master_keyfile_name, server_side=True)
   thread = threading.Thread(target = httpsd.serve_forever)
   thread.daemon = True
 
@@ -261,8 +288,9 @@ def main():
     print (server_version)
     print ("current version ")
     print (current_version)
-    if ret == True and int(server_version) > current_version:
+    if ret == True and int(server_version) > int(current_version):
       image_down()
+      #sign_image("./msg.bin")
       ret = firmware_update()
     else :
       continue
